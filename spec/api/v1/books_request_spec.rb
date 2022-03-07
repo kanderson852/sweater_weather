a6e0d770a174301d640429efe42b734af809b29e
Kelly Anderson <kean9528@colorado.edu>
2022-03-07 10:34:21 -0700

    add more robust testing for request spec

--- spec/api/v1/books_request_spec.rb
+++ spec/api/v1/books_request_spec.rb
@@ -6,10 +6,16 @@ describe "Book search API" do
 
     get '/api/v1/book-search?location=denver,co&quantity=5'
 
-    books = JSON.parse(response.body, symbolize_names: true)
+    books_response = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    expect(books).to be_a(Hash)
-    expect(books).to have_key(:data)
-    
+    expect(books_response).to be_a(Hash)
+    expect(books_response).to have_key(:data)
+    expect(books_response[:data]).to have_key(:attributes)
+    expect(books_response[:data][:attributes][:destination]).to eq("denver,co")
+    expect(books_response[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(books_response[:data][:attributes][:total_books_found]).to eq(41867)
+    expect(books_response[:data][:attributes][:books].count).to eq(5)
+    expect(books_response[:data][:attributes][:forecast]).to_not have_key(:humidity)
+    expect(books_response[:data][:attributes][:books][0]).to_not have_key(:publish_date)
   end
 end
