eb9a72a6531b3edf5d7fa0f4763c9dd35c088ac5
Kelly Anderson <kean9528@colorado.edu>
2022-03-08 16:18:59 -0700

    edge case testing

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,17 +1,26 @@
 class RoadTripFacade
   def self.road_trip(origin, destination)
     trip = RoadTripService.formatted_data(origin, destination)
-    time = trip[:travel_time]
-    hours = time.slice(0..1)
-    minutes = time.slice(3..4)
-    data = {
-      start_city: origin,
-      end_city: destination,
-      travel_time: "#{hours} hours, #{minutes} minutes",
-      weather_at_eta: weather_info(origin, destination, hours)
-    }
-
-    RoadTrip.new(data)
+    if trip[:travel_time] != 'impossible'
+      time = trip[:travel_time]
+      hours = time.slice(0..1)
+      minutes = time.slice(3..4)
+      data = {
+        start_city: origin,
+        end_city: destination,
+        travel_time: "#{hours} hours, #{minutes} minutes",
+        weather_at_eta: weather_info(origin, destination, hours)
+      }
+      RoadTrip.new(data)
+    else
+      data = {
+        start_city: origin,
+        end_city: destination,
+        travel_time: "impossible",
+        weather_at_eta: ''
+      }
+      RoadTrip.new(data)
+    end
   end
 
   def self.weather_info(origin, destination, hours)
